fe3360b95e8521559756334f1c200b3d00e1a5df
Leon Chou <leon.chou@nyu.edu>
2025-01-14 14:17:15 -0500

    index

--- index.sh
+++ index.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+set -uxo pipefail
 
 which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     brew update
 fi
